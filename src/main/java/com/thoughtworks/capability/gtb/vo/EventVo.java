7188eca280bb9fdba80fc6263b00bd2043cb18ea
KyleXu <kyle_x@foxmail.com>
2020-11-07 01:05:02 +0800

    [XTH]: post data to Creat

--- src/main/java/com/thoughtworks/capability/gtb/vo/EventVo.java
+++ src/main/java/com/thoughtworks/capability/gtb/vo/EventVo.java
@@ -1,6 +1,14 @@
 package com.thoughtworks.capability.gtb.vo;
 
+import java.io.IOException;
 import java.util.Date;
+
+import com.fasterxml.jackson.annotation.JsonUnwrapped;
+import com.fasterxml.jackson.annotation.JsonValue;
+import com.fasterxml.jackson.core.JsonGenerator;
+import com.fasterxml.jackson.databind.SerializerProvider;
+import com.fasterxml.jackson.databind.annotation.JsonSerialize;
+import com.fasterxml.jackson.databind.ser.std.StdSerializer;
 import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
@@ -12,7 +20,22 @@ public class EventVo {
 
   private String id;
   private String name;
+  @JsonValue
   private EventType type;
+  @JsonSerialize(using = CustomDateSerialize.class)
   private Date time;
+  @JsonUnwrapped
   private UserVo user;
+}
+
+class CustomDateSerialize extends StdSerializer<Date> {
+
+  protected CustomDateSerialize() {
+    super(Date.class);
+  }
+
+  @Override
+  public void serialize(Date date, JsonGenerator gen, SerializerProvider provider) throws IOException {
+    gen.writeNumber(date.getTime());
+  }
 }
