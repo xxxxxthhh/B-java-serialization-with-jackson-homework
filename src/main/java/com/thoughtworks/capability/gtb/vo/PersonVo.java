eb925f51594a69d0526b2d6d9674af106232ef76
KyleXu <kyle_x@foxmail.com>
2020-11-07 01:17:34 +0800

    [XTH]: finish problem2

--- src/main/java/com/thoughtworks/capability/gtb/vo/PersonVo.java
+++ src/main/java/com/thoughtworks/capability/gtb/vo/PersonVo.java
@@ -1,5 +1,6 @@
 package com.thoughtworks.capability.gtb.vo;
 
+import com.fasterxml.jackson.annotation.JsonInclude;
 import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
@@ -12,5 +13,13 @@ public class PersonVo {
   private String id;
   private Integer age;
   private String name;
+  @JsonInclude(JsonInclude.Include.NON_NULL)
   private String hobby;
+
+  public Integer getAge() {
+      if (age == null){
+        return 0;
+      }
+      return this.age;
+  }
 }
